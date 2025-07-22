d6dde4b2e6117ce0584693a81dc51d3645ecb83a
jonas majila <118437218+jonasMJL997@users.noreply.github.com>
2025-07-23 00:07:39 +0200

    Update example_repo.py

---
+++ example_repo.py
@@ -0,0 +1,4 @@
+#example_repo.py
+# this is a test file to check is git is working correctly and will push to the remote repository
+print("Git is installed and working  correctly")
+print("This is a test file in the example_repo repository.")
